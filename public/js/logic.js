46945019758a28602cbba751cdc443878132ecde
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-01-16 23:06:26 +0300

    data is being collected from the form and displayed on the console

--- public/js/logic.js
+++ public/js/logic.js
@@ -2,7 +2,19 @@ const url = "http://localhost:3000"
 const form = document.getElementById('signupForm')
 if (form) {
     form.addEventListener("submit", (e) => {
-        alert("Testing again")
-        // e.preventDefault()
+        // alert("Testing again")
+        e.preventDefault()
+        userSignup()
     })
 }
+
+const userSignup = () => {
+    const fullnames = document.getElementById('signup-name').value
+    const username = document.getElementById('signup-username').value
+    const email = document.getElementById('signup-email').value
+    const password = document.getElementById('signup-password').value
+
+    console.log(`${fullnames} and ${email} are right here`)
+}
+
+// userSignup()

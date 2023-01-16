573d8db40d28234c1b486f7b83970fca2fc6cead
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-01-16 22:57:20 +0300

    logic.js file has been linked to the signup.ejs file and the event listender functionality is working

---
+++ public/js/logic.js
@@ -0,0 +1,8 @@
+const url = "http://localhost:3000"
+const form = document.getElementById('signupForm')
+if (form) {
+    form.addEventListener("submit", (e) => {
+        alert("Testing again")
+        // e.preventDefault()
+    })
+}
